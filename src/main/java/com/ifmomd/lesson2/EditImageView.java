9ffd09d2e0bf0b4b20e7e51d104e36750bcc2182
Vladimir Skipor <vladimirskipor@gmail.com>
2013-09-19 21:39:27 +0400

    algorithms are complete

--- src/main/java/com/ifmomd/lesson2/EditImageView.java
+++ src/main/java/com/ifmomd/lesson2/EditImageView.java
@@ -58,6 +58,9 @@ public class EditImageView extends View {
         }
 
         public void changeBrightness(double x) {
+            if (x < 1.) {
+                throw new IllegalArgumentException("x must be >= 0.");
+            }
             int newColor;
             int tmpColor;
             for (int i = 0; i < pixels.length; i++) {
@@ -78,6 +81,9 @@ public class EditImageView extends View {
         }
 
         public void fastSqueezeWidth(double k) {
+            if (k < 1.) {
+                throw new IllegalArgumentException("k must be >= 1.");
+            }
             int newWidth = (int) (width / k);
             int[] newPixels = new int[height * newWidth];
             int[] remains = new int[newWidth];
@@ -98,6 +104,9 @@ public class EditImageView extends View {
 
 
         public void fastSqueezeHeight(double k) {
+            if (k < 1.) {
+                throw new IllegalArgumentException("k must be >= 1.");
+            }
             int newHeight = (int) (height / k);
             int[] newPixels = new int[newHeight * width];
             int[] remains = new int[newHeight];
@@ -124,26 +133,33 @@ public class EditImageView extends View {
 
 
         public void squeezeWidth(double k) { // middle color of overlapped pixels
+            if (k < 1.) {
+                throw new IllegalArgumentException("k must be >= 1.");
+            }
             int newWidth = (int) (width / k);
             int[] newPixels = new int[height * newWidth];
             int[] remains = new int[newWidth];
             int[] temporaryRed = new int[newWidth];
             int[] temporaryGreen = new int[newWidth];
             int[] temporaryBlue = new int[newWidth];
+            int[] cellSizes = new int[newWidth];
 
-//            int[] temporaryColor = new int[newWidth];
 
             for (int i = 0; i < remains.length; i++) {
                 remains[i] = (int) (k * i);
             }
+            for (int i = 0; i < newWidth; i++) {
+                cellSizes[i] = ((i == newWidth - 1 ? width : remains[i + 1]) - remains[i]);
+            }
+
+
+
 
             for (int i = 0, y = 0, p = 0; i < height; i++, y += width, p += newWidth) {
 
                 for (int j = 0; j < newWidth; j++) {
 
-                    for (int l = remains[j], r = (j == newWidth - 1 ? width : remains[j + 1]); l < r; l++) {
-//                        temporaryColor[j] += pixels[y + l] & noAlphaMask;
-
+                    for (int l = remains[j], r = l + cellSizes[j]; l < r; l++) {
                         temporaryRed[j] += (pixels[y + l] & maskRed);
                         temporaryGreen[j] += (pixels[y + l] & maskGreen);
                         temporaryBlue[j] += (pixels[y + l] & maskBlue);
@@ -151,16 +167,12 @@ public class EditImageView extends View {
                 }
 
                 for (int j = 0; j < newWidth; j++) {        //do same operations in group
-
-//                    if ((remains[j] - (j == 0 ? 0 : remains[j - 1])) == 0) {
-//                        System.out.println("ASSSSS!!!!!");
-//                    }
-                    final int cellSize = ((j == newWidth - 1 ? width : remains[j + 1]) - remains[j]);
+//
 
 
-                    newPixels[p + j] = ((temporaryRed[j] / cellSize) & maskRed
-                            | (temporaryGreen[j] / cellSize) & maskGreen
-                            | temporaryBlue[j] /cellSize) // maskBlue don't needed
+                    newPixels[p + j] = ((temporaryRed[j] / cellSizes[j]) & maskRed
+                            | (temporaryGreen[j] / cellSizes[j]) & maskGreen
+                            | temporaryBlue[j] / cellSizes[j]) // maskBlue don't needed
                             | maxAlpha;
                 }
                 Arrays.fill(temporaryRed, 0);       //fill is quicker than relocate
@@ -175,7 +187,10 @@ public class EditImageView extends View {
             pixels = newPixels;
         }
 
-        public void squeezeHeight(double k) {
+        public void squeezeHeight(double k) {  // middle color of overlapped pixels
+            if (k < 1.) {
+                throw new IllegalArgumentException("k must be >= 1.");
+            }
             int newHeight = (int) (height / k);
             int[] newPixels = new int[newHeight * width];
             int[] remains = new int[newHeight];
@@ -183,18 +198,40 @@ public class EditImageView extends View {
                 remains[i] = (int) (k * i);
             }
 
+            int[] cellSizes = new int[newHeight];
+            for (int i = 0; i < newHeight; i++) {
+                cellSizes[i] = ((i == newHeight - 1 ? height : remains[i + 1]) - remains[i]);
+
+            }
+
+
             int[] temporaryRed = new int[width];
             int[] temporaryGreen = new int[width];
             int[] temporaryBlue = new int[width];
 
-            for (int i = 0, p = 0; i < newHeight; i++, p += width) {
+            for (int i = 0, p = 0, y = 0; i < newHeight; i++, p += width) {
 
-//                for(int l = remains[i]; )
-//
-//                final int y = remains[i] * width;
-//                for (int j = 0; j < width; j++) {
-//                    newPixels[p + j] = pixels[y + j];
-//                }
+                for (int l = remains[i], r = l + cellSizes[i]; l < r; l++, y += width) {
+                    for (int j = 0; j < width; j++) {
+                        temporaryRed[j] += (pixels[y + j] & maskRed);
+                        temporaryGreen[j] += (pixels[y + j] & maskGreen);
+                        temporaryBlue[j] += (pixels[y + j] & maskBlue);
+
+                    }
+                }
+
+                for (int j = 0; j < width; j++) {        //do same operations in group
+
+
+                    newPixels[p + j] = ((temporaryRed[j] / cellSizes[i]) & maskRed
+                            | (temporaryGreen[j] / cellSizes[i]) & maskGreen
+                            | temporaryBlue[j] / cellSizes[i]) // maskBlue don't needed
+                            | maxAlpha;
+                }
+                Arrays.fill(temporaryRed, 0);       //fill is quicker than relocate
+                Arrays.fill(temporaryGreen, 0);
+                Arrays.fill(temporaryBlue, 0);
+
 
             }
             height = newHeight;
@@ -202,6 +239,10 @@ public class EditImageView extends View {
 
         }
 
+        public void squeeze(double k) {
+            squeezeHeight(k);
+            squeezeWidth(k);
+        }
 
 
     }
@@ -224,13 +265,11 @@ public class EditImageView extends View {
         Image image = new Image(sourceBitmap);
         image.changeBrightness(BRIGHTNESS_CHANGE);
         image.rotateLeft();
-//        image.fastSqueeze(1.83);
-        image.squeezeWidth(3.3);
+        image.squeeze(1.83);
 
 
         canvas.drawBitmap(image.getBitmap(), 0, 0, null);
 
-//        canvas.drawBitmap(sourceBitmap, 0, 0, null);
 
     }
 }
